0269bc560f4d37f27d45e1f0684371dd6c11c2f7
SFjapan <fs12265669@gmail.com>
2024-02-01 15:28:12 +0900

    commit

--- app/components/tag.tsx
+++ app/components/tag.tsx
@@ -2,7 +2,6 @@ import "tailwindcss/tailwind.css"
 import Link from "next/link";
 import { tags } from "../data/tags";
 export const TagList = ()=>{
-     
     return(
         <div className="flex flex-col ">
             <Link href={{pathname:'/',query:{tag:``}}} className="bg-blue-300 hover:bg-blue-500 my-1">すべて</Link>
