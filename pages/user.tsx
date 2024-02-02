0bec7d0681ca9f696024ef90d62802313b3ce8a6
SFjapan <fs12265669@gmail.com>
2024-02-02 11:58:02 +0900

    ブログの削除できた

--- pages/user.tsx
+++ pages/user.tsx
@@ -1,8 +1,8 @@
 import "tailwindcss/tailwind.css"
-import { addDoc, doc, getDoc, setDoc, getDocs, collection } from "firebase/firestore";
+import { addDoc, doc, getDoc, setDoc, getDocs, collection, query, where,deleteDoc,onSnapshot } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { firebaseApp, firestore } from "@/lib/firebase/config";
-import {  useState } from "react";
+import {  useEffect, useState } from "react";
 import { useRouter } from "next/router";
 export default function User() {
     //ユーザー情報
@@ -13,6 +13,31 @@ export default function User() {
     const auth = getAuth(firebaseApp);
     const Uid: string | undefined = auth.currentUser?.uid;
     const router = useRouter();
+    const [posts, setPosts] = useState([{ id:'',username: '', title: '', tag: '', body: '', date: '', postId: 0, like: 0, comments: [{ body: "", date: "", parentId: "", userName: "" }]||null }]);
+    const [snapshoted,setSnapshoted] = useState(false);
+    useEffect(()=>{
+        const getPosts = async() =>{
+            const querySnapshot = await getDocs(collection(firestore,'users'));
+            //documentID判別
+            let documentID: string = "";
+            let prevName : string ="";
+            querySnapshot.forEach((doc) => {
+                //Uidが同じならdocumentID更新
+                if (doc.data().Uid == Uid) {
+                    documentID = doc.id;
+                    prevName = doc.data().userName;
+                }
+            })
+        
+            const q = query(collection(firestore,'blog'),where("userName", "==",prevName));
+            const docs =await getDocs(q); 
+            const postsData = docs.docs.map((doc) => ({id:doc.id, username: doc.data().userName, title: doc.data().title, tag: doc.data().tag, body: doc.data().body, date: doc.data().date, postId: doc.data().postId, like: doc.data().like, comments: doc.data().comments }));
+            setPosts(postsData);
+            setSnapshoted(false);
+        }
+        getPosts();
+    },[snapshoted,Uid])
+    
     const postUser = async () => {
         try {
             // 必要なすべてのフィールドが記入されていることを確認
@@ -87,7 +112,6 @@ export default function User() {
 
             }
         })
-        console.log(postdocs);
         postdocs.forEach(async(element,index) => {
             const targetdoc =doc(firestore,'blog',element)
             const updatedoc = await getDoc(targetdoc);
@@ -124,6 +148,22 @@ export default function User() {
         });
     }
 
+    const deletePost = async(e:any)=>{
+        const yes = window.confirm("削除しますか?")
+        if(!yes)return;
+        const parentID = e.target.closest(".id").id;
+        const targetdoc = doc(firestore, 'blog', parentID);
+        await deleteDoc(targetdoc);
+    }
+
+    useEffect(()=>{
+        try{
+            onSnapshot(collection(firestore,'blog'),(snapshot)=>{
+                setSnapshoted(true);
+            })
+        }catch(error){console.log(error)}
+    },[])
+
 
 
     return (
@@ -141,6 +181,21 @@ export default function User() {
                 <option value="なし">なし</option>
             </select>
             <button className="" onClick={() => postUser()}>登録</button>
+            {posts?
+                <div className="flex flex-col">
+                    {posts.map((e,index)=>(
+                        <div key={index} className="border-2 border-gray-200 rounded-md my-3">
+                            <div className="id flex relative" id={e.id}>
+                                {e.username}<br/>
+                                <button className="absolute right-2 text-red-500" onClick={(e)=>deletePost(e)}>削除</button>
+                            </div>
+                            <pre>{e.body}</pre>
+                            <p className="text-right">{e.date}</p>
+                        </div>
+                        
+                    ))}
+                </div>:
+                <></>}
         </div>
     );
 }
